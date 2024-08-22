dc92e55b836d66cf4f80189eeabb599cbf6813dc
azrubael <i0638464000@gmail.com>
2024-08-22 13:21:48 +0300

    Updated -- 'scripts/nodejs-setup.sh' with a minor correction.

--- scripts/nodejs-setup.sh
+++ scripts/nodejs-setup.sh
@@ -6,7 +6,6 @@ sudo apt install -y curl
 curl -O https://nodejs.org/dist/v7.8.0/node-v7.8.0-linux-x64.tar.xz
 tar -xf node-v7.8.0-linux-x64.tar.xz
 sudo mv node-v7.8.0-linux-x64 /usr/local/node
-sudo cp -rp node-v7.8.0-linux-x64 /usr/local/node
 sudo ln -s /usr/local/node/bin/node /usr/local/bin/node
 sudo ln -s /usr/local/node/bin/npm /usr/local/bin/npm
 echo ""
